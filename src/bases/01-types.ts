7db9601baff94b3c312de6c41aca5f7d31d4f00d
Juan Gómez <gomezj01@stepstone.com>
2023-10-04 15:25:33 -0600

    Tipos de datos

---
+++ src/bases/01-types.ts
@@ -0,0 +1,11 @@
+export const name = 'Juan';
+export const lastName: string = 'Gómez'
+export const surName: string | undefined = 'Carlos'
+
+export const age: number = 34
+export const isValid: boolean = true
+
+export const fullName: string = `
+	${ name } ${ surName } ${ lastName }
+		'FullStack .NET Developer'
+`
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
+import { name, lastName } from './bases/01-types.ts'
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -19,6 +21,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
+	<div>
+		${ name } ${ lastName }
+	</div>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
