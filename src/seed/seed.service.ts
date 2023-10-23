eb432a7f6964b0fb7d762a8d63eb7fa02f77376f
Juan Gómez <gomezj01@stepstone.com>
2023-10-23 14:09:53 -0600

    Se guarda en DB response del API

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,22 +2,26 @@ import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { config } from '../config';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from '../pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
+	constructor(private pokemonService: PokemonService) {}
+
 	private readonly axios: AxiosInstance = axios;
 	private readonly conf = config();
 
 	async executeSeed() {
 		const { data } = await this.axios.get<PokeResponse>(
-			`${this.conf.pokeApiUrl}pokemon?limit=5`,
+			`${this.conf.pokeApiUrl}pokemon?limit=10`,
 		);
 
-		data.results.forEach(({ name, url }) => {
+		await data.results.forEach(async ({ name, url }) => {
 			const segments = url.split('/');
 			const pokemonNumber: number = +segments.at(-2);
 
 			console.log({ name, pokemonNumber });
+			await this.pokemonService.create({ name, number: pokemonNumber });
 		});
 		return data.results;
 	}
