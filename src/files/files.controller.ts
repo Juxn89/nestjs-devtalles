4ad39580a1332f225d259776c128e03d6f638c1d
Juan Gómez <gomezj01@stepstone.com>
2023-11-01 13:40:58 -0600

    Validación y guardar archivos en local storage

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FilesService } from './files.service';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
+import { FilesService } from './files.service';
+import { FileFilter, fileNamer } from './helpers/';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
 	@Post('product')
-	@UseInterceptors(FileInterceptor('file'))
-	uploadFile(
-		@UploadedFile() file: Express.Multer.File
-	) {
-		
+	@UseInterceptors(FileInterceptor('file', { 
+		fileFilter: FileFilter,
+		storage: diskStorage({ destination: './static/products', filename: fileNamer })
+	}))
+	uploadFile( @UploadedFile() file: Express.Multer.File ) {		
+		if(!file)
+			throw new BadRequestException(`Make sure that file is a image`)
+
+		console.log(file)
 		return {
 			file: file.originalname
 		};
---
+++ src/files/helpers/index.ts
@@ -0,0 +1,2 @@
+export { FileFilter } from "./fileFilter.helper";
+export { fileNamer } from "./fileNamer.helper";
