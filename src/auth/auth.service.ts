8dc5e69f3dbeefdd856e09c700a23dffd4a869ed
Juan Gómez <gomezj01@stepstone.com>
2023-11-06 15:55:20 -0600

    Encripctación de contraseña y proceso de login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt'
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+
 
 import { User } from './entities/users.entity';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto/';
 import dbCodeErrors from 'src/common/dbCodeErrors';
 
 @Injectable()
@@ -15,8 +17,15 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
 		try {
-			const user = await this.userRepository.create(createUserDto)
+			const { password, ...userData } = createUserDto
+
+			const user = await this.userRepository.create({
+				...userData,
+				password: bcrypt.hashSync(password, 10)
+			})
 			await this.userRepository.save(user);
+
+			delete user.password;
 
 			return user;
 		} catch (error) {
@@ -24,10 +33,30 @@ export class AuthService {
 		}
   }
 
-	private handleDbError = (error: any): never => {
-		if(error.code === dbCodeErrors.DUPLICATE_KEY_CONSTRAINT) {
-			throw new BadRequestException(`${ error.detail }`)
+	async login(loginUserDto: LoginUserDto) {
+		try {
+			const { email, password } = loginUserDto;
+			const user = await this.userRepository.findOne({ 
+				where: { email },
+				select: { email: true, password: true }
+			})
+
+			if(!user)
+				throw new NotFoundException(`Credentials are not valid`)
+			
+			if(!bcrypt.compareSync(password, user.password))
+				throw new NotFoundException(`Credentials are not valid`)
+			
+			return user;
+
+		} catch (error) {
+			this.handleDbError(error)
 		}
+	}
+
+	private handleDbError = (error: any): never => {
+		if(error.code === dbCodeErrors.DUPLICATE_KEY_CONSTRAINT)
+			throw new BadRequestException(`${ error.detail }`)
 
 		console.error(error)
 		throw new InternalServerErrorException(`Please check server logs`);
