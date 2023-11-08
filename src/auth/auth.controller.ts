9e2642a277463b6b7db59956a60c4c2982d8f56d
Juan Gómez <gomezj01@stepstone.com>
2023-11-08 10:46:42 -0600

    Implementación de Guards en rutas privadas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto/';
@@ -16,4 +17,13 @@ export class AuthController {
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
   }
+
+	@Get('private')
+	@UseGuards(AuthGuard())
+	privateRoute() {
+		return {
+			ok: true,
+			menssage: 'Hi from a private route'
+		}
+	}
 }
