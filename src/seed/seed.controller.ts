17b83a676ec215c182faf1d1383fbc19184228cb
Juan Gómez <gomezj01@stepstone.com>
2023-11-14 10:26:28 -0600

    Limpieza de referencias no usadas

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { SeedService } from './seed.service';
-import { Auth, GetUser } from '../auth/decorators';
-import { ValidRoles } from '../auth/interfaces';
-import { User } from '../auth/entities/users.entity';
 
 @Controller('seed')
 export class SeedController {
