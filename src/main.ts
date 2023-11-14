e77e8bd9bf6a919080494e66c5d96f59fbb22259
Juan Gómez <gomezj01@stepstone.com>
2023-11-14 13:11:31 -0600

    Instalación y configuración del Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -13,6 +14,15 @@ async function bootstrap() {
 		whitelist: true,
 		forbidNonWhitelisted: true
 	}))
+	
+	const config = new DocumentBuilder()
+		.setTitle('Teslo RESTFul API')
+		.setDescription('Teslo shop endpoints')
+		.setVersion('1.0')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document)
 
   await app.listen(process.env.PORT);
 	logger.log(`App running on port ${ process.env.PORT }`);
