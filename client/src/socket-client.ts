66472e1816b273818e0513e4f9fb707bda2f214d
Juan Gómez <gomezj01@stepstone.com>
2023-11-15 11:56:24 -0600

    Detectar conexión y desconexión

--- client/src/socket-client.ts
+++ client/src/socket-client.ts
@@ -1,7 +1,24 @@
 import { Manager } from 'socket.io-client'
+import { Socket } from 'socket.io';
 
 export const connectToServer = () => {
 	const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
 
 	const socket = manager.socket('/')
+
+	addListerners(socket);
+}
+
+const addListerners = (socket: Socket) => {
+	const serverStatusLabel = document.querySelector('#serverStatus')!;
+
+	socket.on('connect', () => {
+		console.log('Connected! :)')
+		serverStatusLabel.innerHTML = 'Connected';
+	})
+	
+	socket.on('disconnect', () => {
+		console.log('Disconnected! :(')
+		serverStatusLabel.innerHTML = 'Disconnected';
+	})
 }
